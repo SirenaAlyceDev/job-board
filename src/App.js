5d14f9ebde3c29765cfb57bfd34db5b66927b869
Sirena Gandy <sirena@sirenaalyce.com>
2021-04-14 19:15:59 -0400

    added employer and applicant signup component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import LandingPage from './components/LandingPageComponent'
 import EmployerComponent from './components/EmployerComponent'
 import './App.css';
 import Login from './components/LoginComponent';
+import EmployerSignUp from './components/EmployerSignUpComponent';
+import ApplicantSignup from './components/ApplicantSignUpComponent';
 
 function App() {
   return (
     // <LandingPage />
     // <EmployerComponent />
-    <Login />
+    // <Login />
+    // <EmployerSignUp />
+    <ApplicantSignup />
   );
 }
 
---
+++ src/components/ApplicantSignUpComponent.js
@@ -0,0 +1,113 @@
+import React from "react";
+import {
+  Button,
+  TextField,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  Checkbox,
+  FormControlLabel,
+  Link,
+} from "@material-ui/core";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://sirenaalyce.com/">
+        SirenaAlyce.com
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+function ApplicantSignup() {
+  return (
+    <Container component="main" maxWidth="xs">
+      <div>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form  noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+
+export default ApplicantSignup;
